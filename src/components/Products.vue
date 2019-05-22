e2a0aaad5ada02f2f02b2b7a30019c64d89dd806
vmirella <vmirella_17@hotmail.com>
2019-05-22 16:01:43 -0500

    cambios de maquetacion

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,40 +1,38 @@
 <template>
-  <v-flex xs12 sm8>
-    <!-- <v-card class="grey darken-3"> -->
-      <h2 class="pt-2" style="font-weight: 300; font-size: 26px; color: white; text-align: center; color: white;">NUESTRAS CATEGORÍAS</h2>
-      <v-container fluid grid-list-md class="pt-1">
-        <v-layout row wrap>
-          <v-flex
-            v-for="product in products"
-            :key="product.nombre"
-            v-bind="{ [`xs4`]: true }"
-          >
-            <v-card>
-              <router-link :to="{ path: '/detailproducts', query: { product: product.nombre }}">
-                <v-img
-                  :src="`https://pruebas.co.pe/carrito/${product.imagen}`"
-                  height="400px"
+  <!-- <v-card class="grey darken-3"> -->
+    <v-content grid-list-md justify-center class="mt-5 mb-5" style="max-width: 1268px; margin: 0 auto">
+      <v-layout row wrap justify-center>
+        <v-flex
+          v-for="product in products"
+          :key="product.nombre"
+          xs12 sm12 md3
+          class="pa-3"
+        >
+          <v-card>
+            <router-link :to="{ path: '/detailproducts', query: { product: product.nombre }}">
+              <v-img
+                :src="`https://pruebas.co.pe/carrito/${product.imagen}`"
+                height="400px"
+              >
+                <v-container
+                  fill-height
+                  fluid
+                  pa-2
                 >
-                  <v-container
-                    fill-height
-                    fluid
-                    pa-2
-                  >
-                  </v-container>
-                </v-img>
-              </router-link>
+                </v-container>
+              </v-img>
+            </router-link>
 
-              <v-card-title primary-title>
-                <div>
-                  <router-link :to="{ path: '/detailproducts', query: { product: product.nombre }}"><h3 class="headline mb-0">{{product.nombre}}</h3></router-link>
-                </div>
-              </v-card-title>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    <!-- </v-card> -->
-  </v-flex>
+            <v-card-title primary-title>
+              <div>
+                <router-link :to="{ path: '/detailproducts', query: { product: product.nombre }}"><h3 class="headline mb-0">{{product.nombre}}</h3></router-link>
+              </div>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-content>
+  <!-- </v-card> -->
 </template>
 
 <script>
