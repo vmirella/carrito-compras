63a30c2e5808946bc82dd7667bc73a350b3ac600
Virginia Mirella Contreras Villafuerte <vmirella_17@hotmail.com>
2026-04-23 00:03:28 -0500

    feat: switch from web history to hash history mode in Vue Router

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import DetalleProducto from '@/views/DetalleProducto.vue'
 import Products from '@/views/Products.vue'
 import Login from '@/views/Login.vue'
@@ -34,7 +34,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
